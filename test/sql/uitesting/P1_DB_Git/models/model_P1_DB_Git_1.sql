010915b8069c674b4da03b82a450da5d477ea21b
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2023-04-14 08:55:37 +0000

    🔁 Modified     - Model        - model_P1_DB_Git_2        - model_P1_DB_Git_1        - model_P1_DB_Git_3

--- test/sql/uitesting/P1_DB_Git/models/model_P1_DB_Git_1.sql
+++ test/sql/uitesting/P1_DB_Git/models/model_P1_DB_Git_1.sql
@@ -21,7 +21,7 @@ Reformat_1 AS (
     c_boolean AS c_boolean,
     c_array AS c_array,
     c_struct AS c_struct,
-    concat({{ var('v_project_int_p1')}}, {{ P1_DB_Git.qa_concat_p1('c_string', v_int_p1) }}) AS c_macro
+    concat({{ var('v_project_int_p1')}}, {{ P1_DB_Git.qa_concat_p1('c_string', "'test random'") }}) AS c_macro
   
   FROM all_type_parquet AS in0
 
